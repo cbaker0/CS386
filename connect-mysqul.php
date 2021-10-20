c25d30cdbffa55e456f6bba3cdccde9a42da3a84
Alenn Wright <bowserko123@gmail.com>
2021-10-20 12:13:34 -0700

    datatest

---
+++ connect-mysqul.php
@@ -0,0 +1,10 @@
+<?php
+
+DEFINE ('DB_USER','u136679073_agw73');
+DEFINE ('DB_PSWD','@Agwri584');
+DEFINE ('DB_HOST','thetataunau.online');
+DEFINE ('DB_NAME','u136679073_AlennData');
+
+$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+
+?>
