4303b710798282f3c4b59ea46fecccc20a39cd33
Alenn Wright <bowserko123@gmail.com>
2021-10-20 13:19:00 -0700

    Setup for Database on host

--- connect-mysqul.php
+++ connect-mysqul.php
@@ -1,10 +1,34 @@
 <?php
+$servername = "localhost";
+$database = "u136679073_AlennData";
+$username = "u136679073_agw73";
+$password = "@Agwri584";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+echo "<br>";
 
-DEFINE ('DB_USER','u136679073_agw73');
-DEFINE ('DB_PSWD','@Agwri584');
-DEFINE ('DB_HOST','thetataunau.online');
-DEFINE ('DB_NAME','u136679073_AlennData');
+$sqldata = mysqli_query($conn, "SELECT * FROM Pokedex");
 
-$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+echo "<table>";
+echo "<tr><th>No.</th><th>Name</th></tr>";
 
+while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC))
+{
+	echo "<tr><td>";
+	echo $row['No.'];
+	echo "</td><td>";
+	echo $row['Name'];
+	echo "</td></tr>";
+}
+
+echo "</table>";
+
+
+
+mysqli_close($conn);
 ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+<title>Database</title>
+</head>
+<body>
+<?php
+require 'connect-mysqul.php';
+?>
+</body>
+</html>
